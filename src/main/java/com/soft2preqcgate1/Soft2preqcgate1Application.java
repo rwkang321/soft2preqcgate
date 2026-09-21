775af790ec36e403e1f9b9d416341f86679d1d5e
강로원 <rwkang@naver.com>
2026-09-21 14:32:04 +0900

    첫번째

---
+++ src/main/java/com/soft2preqcgate1/Soft2preqcgate1Application.java
@@ -0,0 +1,13 @@
+package com.soft2preqcgate1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Soft2preqcgate1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Soft2preqcgate1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/soft2preqcgate1/Soft2preqcgate1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soft2preqcgate1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Soft2preqcgate1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
